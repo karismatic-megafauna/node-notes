b29901d9018ee3acc1f492b3c67db315dbeb4ccf
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-03-19 13:27:50 -0700

    start on nonote

--- main.js
+++ main.js
@@ -5,38 +5,54 @@ var chalk = require('chalk');
 var request = require('superagent');
 var prompt = require('co-prompt');
 var program = require('commander');
+var jsonfile = require('jsonfile');
+
+var file = './note-template.json';
+
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(Object.keys(obj))
+});
+
+var lolz = jsonfile.readFileSync(file);
+
+console.log(lolz);
 
 program
-  .arguments('<file>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .action(function(file) {
-    co(function *() {
-      var username = yield prompt('username: ');
-      var password = yield prompt.password('password: ');
-      request
-        .post('https://api.bitbucket.org/2.0/snippets/')
-        .auth(username, password)
-        .attach('file', file)
-        .set('Accept', 'application/json')
-        .end(function (err, res) {
-          if (!err && res.ok) {
-            var link = res.body.links.html.href;
-            console.log(chalk.bold.cyan('Snippet created: ') + link);
-            process.exit(0);
-          }
-
-          var errorMessage;
-          if (res && res.status === 401) {
-            errorMessage = "Auth failed! Make sure user and pass are correct.";
-          } else if (err) {
-            errorMessage = err;
-          } else {
-            errorMessage = res.text;
-          }
-          console.error(chalk.red(errorMessage));
-          process.exit(1);
-        });
-    });
-  })
+  .version('0.0.1')
+  .command('new [note]', 'create new note for day')
   .parse(process.argv);
+
+// program
+//   .arguments('<file>')
+//   .option('-u, --username <username>', 'The user to authenticate as')
+//   .option('-p, --password <password>', 'The user\'s password')
+//   .action(function(file) {
+//     co(function *() {
+//       var username = yield prompt('username: ');
+//       var password = yield prompt.password('password: ');
+//       request
+//         .post('https://api.bitbucket.org/2.0/snippets/')
+//         .auth(username, password)
+//         .attach('file', file)
+//         .set('Accept', 'application/json')
+//         .end(function (err, res) {
+//           if (!err && res.ok) {
+//             var link = res.body.links.html.href;
+//             console.log(chalk.bold.cyan('Snippet created: ') + link);
+//             process.exit(0);
+//           }
+
+//           var errorMessage;
+//           if (res && res.status === 401) {
+//             errorMessage = "Auth failed! Make sure user and pass are correct.";
+//           } else if (err) {
+//             errorMessage = err;
+//           } else {
+//             errorMessage = res.text;
+//           }
+//           console.error(chalk.red(errorMessage));
+//           process.exit(1);
+//         });
+//     });
+//   })
+//   .parse(process.argv);
